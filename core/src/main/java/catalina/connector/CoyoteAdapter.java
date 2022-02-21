57a688ce9f114dd7fcfb886b972fcffd3676b3cc
Pyhita <pyhita.dev@gmail.com>
2022-02-21 23:29:26 +0800

    寻找servlet的完整逻辑

--- core/src/main/java/catalina/connector/CoyoteAdapter.java
+++ core/src/main/java/catalina/connector/CoyoteAdapter.java
@@ -1,10 +1,68 @@
 package catalina.connector;
 
+import catalina.Context;
+import catalina.Host;
+import catalina.ServletWrapper;
+import http.Request;
+import http.Response;
+import util.HttpStatus;
+
+import java.util.Map;
+
 /**
- * @Autuor: innthehell
+ * @Autuor: pyhita
  * @Date: 2021/12/22 - 12 - 22 - 22:16
  * @Description: catalina.connector
  * @Version: 1.0
  */
 public class CoyoteAdapter {
+    private Connector connector;
+
+    public void doDispatch(Request request, Response response) throws Exception {
+        for (Host host : connector.getEngine().getHosts()) {
+            // 主机名字对不上，继续遍历
+            // 1 根据host 确定主机
+            if (!host.getName().equals(request.getHost())) continue;
+            // 2 根据请求的URL确定context
+            String url = request.getUrl();
+            for (Map.Entry<String, Context> entry : host.getContextMap().entrySet()) {
+                String path = entry.getKey();
+                Context context = entry.getValue();
+                if (url.startsWith(path)) {
+                    request.setContext(context);
+                    response.setContext(context);
+                    break;
+                }
+            }
+            if (request.getContext() != null) {
+                break;
+            }
+        }
+        // 调用service方法找到servlet
+        service(request, response);
+    }
+
+
+    private void service(Request request, Response response) throws Exception {
+        Context context = request.getContext();
+        if (context != null) {
+            String urlPattern = request.getUrl();
+            // 消除掉uri中的path，得到servlet映射路径
+            if (!"/".equals(context.getPath())) {
+                urlPattern = urlPattern.replaceFirst(context.getPath(), "");
+            }
+
+            // 找到相应的servlet
+            ServletWrapper servletWrapper = context.findServletWrapper(urlPattern);
+            if (servletWrapper != null) {
+                response.setStatus(HttpStatus.OK);
+                servletWrapper.getServlet().service(request, response);
+            } else {
+
+            }
+        }
+
+    }
+
+
 }
